80185fe483f96381376f8485c8df57df2c8ef845
MaxZay <54762301+MaxZay@users.noreply.github.com>
2021-02-15 22:10:26 +0300

    Решил

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n.toString().includes('-')) {
+        n = n.toString().replace('-', '')
+        return Number(n.toString().split('').reverse().join(""))
+    } else {
+        return Number(n.toString().split('').reverse().join(""))
+    }
+
 }
